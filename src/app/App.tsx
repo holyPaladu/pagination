6f080872054058da820d07b69e8d50fea3c77eba
holyPaladu <ironic_paladin@mail.ru>
2025-03-31 19:38:10 +0500

    output in App my looped pagination

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,19 @@
 import "./styles/index.scss";
+import styles from "./App.module.scss";
+import { Pagination } from "../features/Pagination";
 
 function App() {
   return (
     <>
-      <div></div>
+      <main className={styles.main}>
+        <div className={styles.main__wrapper}>
+          <h1 className={styles.main__title}>Pagination</h1>
+          <section className={styles.main__section}>
+            <h2>Looped pagination</h2>
+            <Pagination totalPages={10} isLooped={true} />
+          </section>
+        </div>
+      </main>
     </>
   );
 }
